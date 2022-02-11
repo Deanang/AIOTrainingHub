1aa21099c36a99a23b32744697016eb53dc8a46e
Dean Ang <ngeekeng@gmail.com>
2022-02-11 19:37:55 +0800

    Added Melody
    Added Melody

--- AIO Past Year Questions/2021/melody.py
+++ AIO Past Year Questions/2021/melody.py
@@ -18,8 +18,7 @@ N = None
 # K is the largest number which could be a note.
 K = None
 
-# S contains the sequence of notes forming the song.
-S = [None for x in range(100005)]
+
 
 answer = None
 
@@ -31,14 +30,23 @@ output_file = open("melodyout.txt", "w")
 input_line = input_file.readline().strip()
 N, K = map(int, input_line.split())
 
+# S contains the sequence of notes forming the song.
+S = [None for x in range(N)]
+
 # Read each note in the song.
 for i in range(0, N):
     S[i] = int(input_file.readline().strip())
 
+
 # TODO: This is where you should compute your solution. Store the smallest
 # possible number of notes Melody can change so that her song is nice into the
 # variable answer.
-
+M, m = [None, None, None], [None, None, None]
+for i in range(3):
+    M[i] = S[i::3]
+    m[i] = max(set(M[i]), key = M[i].count)
+answer = len([x for x in M[0] if x != m[0]]) + len([x for x in M[1] if x != m[1]]) + len([x for x in M[2] if x != m[2]])
+print(answer)
 # Write the answer to the output file.
 output_file.write("%d\n" % (answer))
 
